f40651a85e49aa8030e9511cde08243772ef5e95
Thomas <denhollander.thomas@gmail.com>
2021-12-03 19:59:13 +0100

    178

--- src/day3/code_golf.rs
+++ src/day3/code_golf.rs
@@ -1,1 +1,1 @@
-pub fn z(a:&str)->u32{let(mut b,mut c,mut g,mut e,mut u)=([0;13],0,0,0,4096);for a in a.bytes(){c+=1;b[c%13]+=a as usize&1}for a in b.iter_mut(){if 26**a>c+1{g|=u}u>>=1;e|=u}g*(e^g)}
+pub fn z(a:&str)->u32{let(mut b,mut c,mut g,mut e,mut u)=([0;13],0,0,0,4096);for a in a.bytes(){c+=1;b[c%13]+=a as usize&1}for a in 0..13{if 26*b[a]>c+1{g|=u}u>>=1;e|=u}g*(e^g)}
