09ec9c0dd79f7ecdc3ecf9ba8aaddf9ee4f49b80
Thomas <denhollander.thomas@gmail.com>
2021-12-05 18:48:59 +0100

    Day 5 pre-cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,4 @@
-use std::{ops::{Mul, Sub, DivAssign, Add}, collections::HashSet};
+use std::{ops::{Mul, Sub, DivAssign, Add}, collections::{HashSet, HashMap}, str::Lines, cmp::Ordering};
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct Coordinate {
@@ -84,10 +84,199 @@ impl From<(Coordinate, Coordinate)> for LineSegment {
 enum Direction {
     Horizontal,
     Vertical,
-    Other
+    Diagonal
+}
+
+enum Overlap {
+    None,
+    Multi(Coordinate, Vector, i32)
+}
+
+impl Overlap {
+    fn single(coord: Coordinate) -> Overlap {
+        Overlap::Multi(coord, Vector(0, 0), 0)
+    }
+}
+
+impl Iterator for Overlap {
+    type Item = Coordinate;
+    fn next(&mut self) -> Option<Self::Item> {
+        match *self {
+            Overlap::None => None,
+            Overlap::Multi(mut coord, vec, mut len) => {
+                len -= 1;
+                coord = coord + vec;
+                if len == 0 {
+                    *self = Overlap::None;
+                } else {
+                    *self = Overlap::Multi(coord, vec, len);
+                }
+                Some(coord)
+            }
+        }
+    }
+}
+
+fn sort_tuple(a: i32, b: i32) -> (i32, i32) {
+    if a < b {
+        (a, b)
+    } else {
+        (b, a)
+    }
 }
 
 impl LineSegment {
+    /// Sort a line segment in the following way:
+    /// If x stays constant, make y increasing.
+    /// Otherwise, make x increasing.
+    fn sorted(self) -> LineSegment {
+        let inverted = LineSegment {
+            from: self.to,
+            to: self.from
+        };
+        match self.from.x.cmp(&self.to.x) {
+            Ordering::Greater => inverted,
+            Ordering::Equal => {
+                match self.from.y.cmp(&self.to.y) {
+                    Ordering::Greater => inverted,
+                    Ordering::Equal | Ordering::Less => self
+                }
+            },
+            Ordering::Less => self
+        }
+    }
+
+    fn points_of_overlap(&self, other: LineSegment) -> Overlap {
+        // Sort so all lines have increasing x, or y.
+        let first = self.sorted();
+        let second = other.sorted();
+        first.points_of_overlap_sorted(second)
+    }
+
+    fn points_of_overlap_sorted(&self, other: LineSegment) -> Overlap {
+        match (self.direction(), other.direction()) {
+            (Direction::Horizontal, Direction::Horizontal) => {
+                if self.from.y == other.from.y {
+                    let x_start = self.from.x.max(other.from.x);
+                    let x_end = self.to.x.min(other.to.x);
+                    let dx = x_end - x_start;
+                    if dx >= 0 {
+                        Overlap::Multi(Coordinate { x: x_start, y: self.from.y }, Vector(1, 0), dx)
+                    } else {
+                        Overlap::None
+                    }
+                } else {
+                    // Parallel lines
+                    Overlap::None
+                }
+            },
+            (Direction::Vertical, Direction::Vertical) => {
+                if self.from.x == other.from.x {
+                    let y_start = self.from.y.max(other.from.y);
+                    let y_end = self.to.y.min(other.to.y);
+                    let dy = y_end - y_start;
+                    if dy >= 0 {
+                        Overlap::Multi(Coordinate { x: self.from.x, y: y_start }, Vector(0, 1), dy)
+                    } else {
+                        Overlap::None
+                    }
+                } else {
+                    // Parallel lines
+                    Overlap::None
+                }
+            },
+            (Direction::Horizontal, Direction::Vertical) => {
+                other.points_of_overlap_sorted(*self)
+            },
+            (Direction::Vertical, Direction::Horizontal) => {
+                if (self.from.y..=self.to.y).contains(&other.from.y) && (other.from.x..=other.to.x).contains(&self.from.x) {
+                    Overlap::single(Coordinate { x: self.from.x, y: other.from.x })
+                } else {
+                    Overlap::None
+                }
+            },
+            (Direction::Horizontal, Direction::Diagonal) => other.points_of_overlap_sorted(*self),
+            (Direction::Vertical, Direction::Diagonal) => other.points_of_overlap_sorted(*self),
+            (Direction::Diagonal, Direction::Diagonal) => {
+                // Both line segments have possible directions: right & up and right & down.
+                let self_dy = self.to.y - self.from.y;
+                let other_dy = other.to.y - other.from.y;
+                match ((self_dy, *self), (other_dy, other)) {
+                    ((i32::MIN..=0, decreasing_y), (0.., increasing_y)) | ((0.., increasing_y), (i32::MIN..=0, decreasing_y)) => {
+                        // The two lines potentially cross
+                        // The formula for the increasing y is y = a + bx, b = 1.
+                        if decreasing_y.from.y >= increasing_y.from.y + (decreasing_y.from.x - increasing_y.from.x)
+                            && decreasing_y.to.y <= increasing_y.from.y + (decreasing_y.to.x - increasing_y.from.x)
+                            && increasing_y.from.y <= decreasing_y.from.y - (increasing_y.from.x - decreasing_y.from.x)
+                            && increasing_y.to.y >= decreasing_y.from.y - (increasing_y.to.x - decreasing_y.from.x) {
+                                let x=  ((decreasing_y.from.y - decreasing_y.from.x) - (increasing_y.from.y - increasing_y.from.x)) / 2;
+                                let y = (decreasing_y.from.y - decreasing_y.from.x) - x;
+                                Overlap::single(Coordinate {x, y})
+                            } else {
+                                Overlap::None
+                            }
+                    },
+                    _ => {
+                        // Both are increasing or both are decreasing
+                        let slope = Vector(1, self_dy.signum());
+                        let a = self.from.y - self.from.x;
+                        if other.from.y - other.from.x != a {
+                            // Same constant, so crossing
+                            let from_coordinate = if self.from.x > other.from.x {
+                                self.from
+                            } else {
+                                other.from
+                            };
+                            let len = self.to.x.min(other.to.x) - from_coordinate.x;
+                            if len >= 0 {
+                                Overlap::Multi(from_coordinate, slope, len)
+                            } else {
+                                Overlap::None
+                            }
+                        } else {
+                            // Parallel lines
+                            Overlap::None
+                        }
+                    }
+                }
+            },
+            (Direction::Diagonal, Direction::Horizontal) => {
+                let a = self.from.y - self.from.x;
+                let b = (self.to.y - self.from.y).signum();
+                // self follows y = a + bx
+                let other_y = other.from.y;
+                if other.from.x <= self.to.x && other.to.x >= self.from.x // x should overlap
+                && (a + b * other.from.x - other_y) != (a + b * other.to.x - other_y) // The diagonal should be on different sides of the horizontal line for its starting and end point
+                {
+                    let y = other_y;
+                    let x = (y - a) * b; // (y - a) * b is the same as (y - a) / b for b in [1, -1]
+                    Overlap::single(Coordinate {
+                        x, y
+                    })
+                } else {
+                    Overlap::None
+                }
+            }
+            (Direction::Diagonal, Direction::Vertical) => {
+                let a = self.from.y - self.from.x;
+                let b = (self.to.y - self.from.y).signum();
+                // self follows y = a + bx
+                let other_x = other.from.x;
+                if other_x >= self.from.x && other_x <= self.to.x // x should overlap
+                && (a + b * other_x - other.from.y) != (a + b * other_x - other.to.y) // The diagonal should be on different sides of the horizontal line for its starting and end point
+                {
+                    let x = other_x;
+                    let y = a + b * x;
+                    Overlap::single(Coordinate {
+                        x, y
+                    })
+                } else {
+                    Overlap::None
+                }
+            }
+        }
+    }
+
     fn difference_vector(&self) -> Vector {
         Vector::from(self.to) - Vector::from(self.from)
     }
@@ -98,7 +287,7 @@ impl LineSegment {
         } else if self.from.y == self.to.y {
             Direction::Horizontal
         } else {
-            Direction::Other
+            Direction::Diagonal
         }
     }
 
@@ -106,7 +295,7 @@ impl LineSegment {
         matches!(self.direction(), Direction::Horizontal | Direction::Vertical)
     }
 
-    fn points(&self) -> impl Iterator<Item = Coordinate> + '_ {
+    fn points(self) -> impl Iterator<Item = Coordinate> {
         let mut diff = self.difference_vector();
         let gcd = diff.gcd();
         diff /= diff.gcd();
@@ -115,7 +304,8 @@ impl LineSegment {
 }
 
 mod parse {
-    use nom::{sequence::{separated_pair}, character::complete::{digit1, newline}, IResult, combinator::{map, eof}, bytes::complete::tag, branch::alt, multi::separated_list1};
+    use aoc_main::utils::Line;
+    use nom::{sequence::{separated_pair, terminated}, character::complete::{digit1, newline}, IResult, combinator::{map, eof, iterator, opt, ParserIterator}, bytes::complete::tag, branch::alt, multi::separated_list1};
 
     use super::{Coordinate, LineSegment};
 
@@ -131,50 +321,31 @@ mod parse {
         map(separated_pair(coordinate, tag(" -> "), coordinate), LineSegment::from)(input)
     }
 
-    pub(super) fn entire_input(input: &[u8]) -> IResult<&[u8], Vec<LineSegment>> {
-        separated_list1(newline, line_segment)(input)
+    pub(super) fn entire_input<'a>(input: &'a [u8]) -> ParserIterator<&'a [u8], nom::error::Error<&'a [u8]>, impl FnMut(&'a [u8]) -> IResult<&'a [u8], LineSegment>> {
+        iterator(input, terminated(line_segment, opt(newline)))
     }
 }
 
 
 pub fn part_1(input: &str) -> usize {
-    let (_, mut segments) = parse::entire_input(input.as_bytes()).unwrap();
-    segments.retain(LineSegment::is_horizontal_or_vertical);
-
-    let mut points_of_overlap = HashSet::new();
-
-    for (i, segment) in segments.iter().enumerate() {
-        for other_segment in segments.iter().skip(i + 1) {
-            for point_1 in segment.points() {
-                for point_2 in other_segment.points() {
-                    if point_1 == point_2 {
-                        points_of_overlap.insert(point_1);
-                    }
-                }
-            }
-        }
-    }
-    points_of_overlap.len()
+    let points: HashMap<Coordinate, usize> = parse::entire_input(input.as_bytes())
+        .filter(LineSegment::is_horizontal_or_vertical)
+        .flat_map(LineSegment::points)
+        .fold(HashMap::new(), |mut map, point| {
+            map.entry(point).and_modify(|count| *count += 1).or_insert(1);
+            map
+        });
+    points.values().filter(|val| **val > 1).count()
 }
 
-
 pub fn part_2(input: &str) -> usize {
-    let (_, mut segments) = parse::entire_input(input.as_bytes()).unwrap();
-    
-    let mut points_of_overlap = HashSet::new();
-
-    for (i, segment) in segments.iter().enumerate() {
-        for other_segment in segments.iter().skip(i + 1) {
-            for point_1 in segment.points() {
-                for point_2 in other_segment.points() {
-                    if point_1 == point_2 {
-                        points_of_overlap.insert(point_1);
-                    }
-                }
-            }
-        }
-    }
-    points_of_overlap.len()
+    let points: HashMap<Coordinate, bool> = parse::entire_input(input.as_bytes())
+        .flat_map(LineSegment::points)
+        .fold(HashMap::new(), |mut map, point| {
+            map.entry(point).and_modify(|seen| *seen = true).or_insert(false);
+            map
+        });
+    points.values().filter(|val| **val).count()
 }
 
 #[test]
@@ -205,4 +376,16 @@ fn test_part_2_example() {
 0,0 -> 8,8
 5,5 -> 8,2";
     assert_eq!(part_2(input), 12);
+}
+
+#[test]
+fn test_part_1_input() {
+    let input = include_str!("../input/2021/day5.txt");
+    assert_eq!(part_1(input), 7674);
+}
+
+#[test]
+fn test_part_2_input() {
+    let input = include_str!("../input/2021/day5.txt");
+    assert_eq!(part_2(input), 20898);
 }
