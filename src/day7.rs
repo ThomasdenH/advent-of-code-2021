74a5cc32707fd856e25c90b8d698dee92768b126
Thomas den Hollander <denhollander.thomas@gmail.com>
2021-12-07 13:00:00 +0100

    Day 7

---
+++ src/day7.rs
@@ -0,0 +1,75 @@
+use std::char::MAX;
+
+fn parse_numbers(input: &str) -> impl Iterator<Item = usize> + '_ {
+    let mut iter = input
+        .as_bytes()
+        .iter()
+        .copied()
+        .inspect(|&a| debug_assert!(a.is_ascii_digit() || a == b','));
+    std::iter::from_fn(move || {
+        iter.next().map(|x| {
+            (&mut iter)
+                .take_while(|b| *b != b',')
+                // Map to u64, preserve order
+                .fold(usize::from(x - b'0'), |acc, digit| 10 * acc + usize::from(digit - b'0'))
+        })
+    })
+}
+
+fn k_th(input: &mut [usize], k: usize) -> usize {
+    let len = input.len();
+    debug_assert!(len > 0);
+    let pivot = input[0];
+    let pivot_around = input.iter_mut().partition_in_place(|item| *item < pivot);
+    if k < pivot_around {
+        k_th(&mut input[..pivot_around], k)
+    } else if k > pivot_around {
+        k_th(&mut input[(pivot_around + 1)..], k - pivot_around - 1)
+    } else {
+        pivot
+    }
+}
+
+pub fn part_1(input: &str) -> usize {
+    let mut numbers: Vec<_> = parse_numbers(input).collect();
+    let mid_point = numbers.len() / 2;
+    let mid_point_value = k_th(&mut numbers, mid_point);
+    numbers.into_iter()
+        .map(|item| if item > mid_point_value {
+            item - mid_point_value
+        } else {
+            mid_point_value - item
+        }).sum()
+}
+
+pub fn part_2<const MAX_VALUE: usize>(input: &str) -> usize {
+    let mut numbers: Vec<_> = parse_numbers(input).collect();
+    // Guess a mu, find the derivative and increase mu accordingly
+    let min = 0;
+    let max = MAX_VALUE;
+    while min != max {
+        let mu_guess = (min + max) / 2;
+        
+    }
+    min
+}
+
+#[test]
+fn test_part_1_example_input() {
+    let input = "16,1,2,0,4,2,7,1,2,14";
+    assert_eq!(part_1(input), 37);
+}
+
+#[test]
+fn test_part_2_example_input() {
+    let input = "16,1,2,0,4,2,7,1,2,14";
+    assert_eq!(part_2::<20>(input), 5);
+}
+
+#[test]
+fn test_kth() {
+    assert_eq!(k_th(&mut vec![2, 0, 1], 0), 0);
+    assert_eq!(k_th(&mut vec![2, 0, 1], 1), 1);
+    assert_eq!(k_th(&mut vec![2, 0, 1], 2), 2);
+    assert_eq!(k_th(&mut vec![0,5,1,2,3,6,4], 3), 3)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Used for day 7
+#![feature(iter_partition_in_place)]
+
+// Used for day 6
 #![feature(const_eval_limit)]
 #![const_eval_limit = "0"]
 
@@ -7,13 +11,15 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 aoc_main::main! {
     year 2021;
-    day1 => part_1, part_2;
-    day2 => part_1, part_2;
-    day3 => part_1, part_2, part_1_code_golf;
-    day4 => part_1, part_2;
-    day5 => part_1, part_2;
-    day6 => part_1, part_2;
+    //day1 => part_1, part_2;
+    //day2 => part_1, part_2;
+    //day3 => part_1, part_2, part_1_code_golf;
+    //day4 => part_1, part_2;
+    //day5 => part_1, part_2;
+    //day6 => part_1, part_2;
+    day7 => part_1;
 }
