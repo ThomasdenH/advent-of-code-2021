8b2c1ddc27f2e4ffb03006472176badae1902eb5
Thomas <denhollander.thomas@gmail.com>
2021-12-09 20:55:01 +0100

    Run every day again

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,13 @@ mod day9;
 
 aoc_main::main! {
     year 2021;
-    //day1 => part_1, part_2;
-    //day2 => part_1, part_2;
-    //day3 => part_1, part_2, part_1_code_golf;
-    //day4 => part_1, part_2;
-    //day5 => part_1, part_2;
-    //day6 => part_1, part_2;
-    //day7 => part_1, part_2_incrementing_mu, part_2_mean;
-    //day8 => part_1, part_2, part_2_frequency_table;
+    day1 => part_1, part_2;
+    day2 => part_1, part_2;
+    day3 => part_1, part_2, part_1_code_golf;
+    day4 => part_1, part_2;
+    day5 => part_1, part_2;
+    day6 => part_1, part_2;
+    day7 => part_1, part_2_incrementing_mu, part_2_mean;
+    day8 => part_1, part_2, part_2_frequency_table;
     day9 => part_1, part_2;
 }
