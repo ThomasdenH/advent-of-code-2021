a6343b0413409f44a82b79d602f67a5b3c609d2f
Thomas <denhollander.thomas@gmail.com>
2021-12-12 15:55:42 +0100

    Day 12

--- src/day12.rs
+++ src/day12.rs
@@ -2,81 +2,104 @@ use std::collections::HashMap;
 
 struct Graph<'a> {
     nodes: HashMap<&'a str, Node<'a>>,
+    visit_rule: VisitRule
+}
+
+#[derive(Eq, PartialEq, Copy, Clone)]
+enum VisitRule {
+    Single,
+    DoubleOnce
 }
 
 struct Node<'a> {
     neighbours: Vec<&'a str>,
-    allowed_visits: u8,
+    visited: bool,
+    is_small: bool
 }
 
 impl<'a> Graph<'a> {
-    fn add_connection_one_way(&mut self, name: &'a str, name_2: &'a str, max_visits: u8) {
+    fn add_connection_one_way(&mut self, name: &'a str, name_2: &'a str) {
         let is_small = name.chars().all(|c| c.is_ascii_lowercase());
-        let allowed_visits = if is_small { max_visits } else { u8::MAX };
         self
             .nodes
             .entry(name)
             .or_insert_with(|| Node {
                 neighbours: Vec::new(),
-                allowed_visits
+                visited: false,
+                is_small
             })
             .neighbours
             .push(name_2);
     }
 
-    fn new(input: &'a str, max_visits: u8) -> Self {
+    fn new(input: &'a str, visit_rule: VisitRule) -> Self {
         let mut graph = Graph {
             nodes: HashMap::new(),
+            visit_rule
         };
         for line in input.lines() {
             let mut split = line.split('-');
             let node_a_name = split.next().unwrap();
             let node_b_name = split.next().unwrap().trim_end();
-            graph.add_connection_one_way(node_a_name, node_b_name, max_visits);
+            graph.add_connection_one_way(node_a_name, node_b_name);
             if node_b_name != "end" && node_a_name != "start" {
-                graph.add_connection_one_way(node_b_name, node_a_name, max_visits);
+                graph.add_connection_one_way(node_b_name, node_a_name);
             }
         }
         graph
     }
 
-    fn can_visit(&self, node: &str) -> bool {
-        self.nodes.get(node).unwrap().allowed_visits > 0
+    fn is_big_or_unvisited(&self, node: &str) -> bool {
+        let node = self.nodes.get(node).unwrap();
+        !node.is_small || !node.visited
     }
 
-    fn paths_to_end(&mut self, node_str: &str, paths: usize) -> usize {
-        dbg!(node_str, paths);
+    fn paths_to_end(&mut self, node_str: &'a str, paths: usize, can_still_visit_twice: &mut bool) -> usize {
         let node = self.nodes.get_mut(node_str).unwrap();
-        node.allowed_visits -= 1;
+        let used_extra_visit = node.is_small && node.visited;
+        if used_extra_visit {
+            debug_assert!(self.visit_rule == VisitRule::DoubleOnce);
+            debug_assert!(node.visited);
+            debug_assert!(!node.is_small || *can_still_visit_twice == true);
+            *can_still_visit_twice = false;
+        } else {
+            debug_assert!(!node.is_small || !node.visited);
+            node.visited = true;
+        }
         let paths = node
             .neighbours
             .clone()
             .iter()
-            .filter_map(|node| {
-                if *node == "end" {
+            .filter_map(|other_node| {
+                if *other_node == "end" {
                     Some(1)
-                } else if self.can_visit(node) {
-                    Some(self.paths_to_end(node, paths))
+                } else if self.is_big_or_unvisited(other_node) || (self.visit_rule == VisitRule::DoubleOnce && *can_still_visit_twice) {
+                    Some(self.paths_to_end(other_node, paths, can_still_visit_twice))
                 } else {
                     None
                 }
             })
             .sum();
-        self.nodes.get_mut(node_str).unwrap().allowed_visits += 1;
+        if used_extra_visit {
+            *can_still_visit_twice = true;
+        } else {
+            self.nodes.get_mut(node_str).unwrap().visited = false;
+        }
         paths
     }
 
     fn paths_from_start_to_end(&mut self) -> usize {
-        self.paths_to_end("start", 1)
+        let mut can_still_visit_twice = if self.visit_rule == VisitRule::DoubleOnce { true } else { false };
+        self.paths_to_end("start", 1, &mut can_still_visit_twice)
     }
 }
 
 pub fn part_1(input: &str) -> usize {
-    Graph::new(input, 1).paths_from_start_to_end()
+    Graph::new(input, VisitRule::Single).paths_from_start_to_end()
 }
 
 pub fn part_2(input: &str) -> usize {
-    Graph::new(input, 2).paths_from_start_to_end()
+    Graph::new(input, VisitRule::DoubleOnce).paths_from_start_to_end()
 }
 
 #[test]
